e88ee554d43f9d90e7448011098cf271836e3b4a
cris2120 <cris2120@users.noreply.github.com>
2026-08-27 08:15:54 -0500

    actualizacion

--- API/Models/insumos.py
+++ API/Models/insumos.py
@@ -1,6 +1,6 @@
 class Insumos:
 
-    def __init__(self, INS_ID, INS_UUID, INS_CODIGO, INS_NOMBRE, INS_CANTIDAD, INS_PRECIO, INS_ESTADO, INS_USUA_ID, INS_DET_ETC_ID, INS_PROV_ID):
+    def __init__(self, INS_ID, INS_UUID, INS_CODIGO, INS_NOMBRE, INS_CANTIDAD, INS_PRECIO, INS_ESTADO, INS_USUA_ID, INS_PROV_ID, INS_DET_ETC_ID):
         self.INS_ID = INS_ID
         self.INS_UUID = INS_UUID
         self.INS_CODIGO = INS_CODIGO
@@ -9,8 +9,8 @@ class Insumos:
         self.INS_PRECIO = INS_PRECIO
         self.INS_ESTADO = INS_ESTADO
         self.INS_USUA_ID = INS_USUA_ID
-        self.INS_DET_ETC_ID = INS_DET_ETC_ID
         self.INS_PROV_ID = INS_PROV_ID
+        self.INS_DET_ETC_ID = INS_DET_ETC_ID
 
     def to_dic(self):
         return {
@@ -22,6 +22,6 @@ class Insumos:
             "precio": self.INS_PRECIO,
             "estado": self.INS_ESTADO,
             "usua_id": self.INS_USUA_ID,
-            "det_etc_id": self.INS_DET_ETC_ID,
-            "prov_id": self.INS_PROV_ID
+            "prov_id": self.INS_PROV_ID,
+            "det_etc_id": self.INS_DET_ETC_ID
         }
--- API/Routes/insumos_bp.py
+++ API/Routes/insumos_bp.py
@@ -1,24 +1,23 @@
 from flask import Blueprint, jsonify
-from Controllers.insumos_controller import cntListInsumos, cntAddInsumos, cntDelInsumos, cntModInsumos
-
+from Controllers.insumos_controller import insumos_controller
 insumos_bp = Blueprint('insumos_bp', __name__)
 
 @insumos_bp.route('/', methods=['GET'])
 def listInsumos():
-    x = cntListInsumos()
+    x = insumos_controller.cntListInsumos()
     return x
 
 @insumos_bp.route('/', methods=['POST'])
 def createInsumos():
-    x = cntAddInsumos
+    x = insumos_controller.cntAddInsumos()
     return x
 
-@insumos_bp.route('/', methods=['PUT'])
-def updateInsumos():
-    x = cntModInsumos
+@insumos_bp.route('/<id>', methods=['PUT'])
+def updateInsumos(id):
+    x = insumos_controller.cntModInsumos(id)
     return x
 
-@insumos_bp.route('/', methods=['DELETE'])
-def deleteInsumos():
-    x = cntDelInsumos
+@insumos_bp.route('/<id>', methods=['DELETE'])
+def deleteInsumos(id):
+    x = insumos_controller.cntDelInsumos(id)
     return x
